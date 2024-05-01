c9002dbe7cb3a876ead06f34a8e40c4222ea672c
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-01 22:24:10 +0600

    improve

--- NeoJS/data/strandtest.js
+++ NeoJS/data/strandtest.js
@@ -1,99 +1,75 @@
 /* jshint undef: true, unused: false, bitwise: false */
 /* globals Pixel, millis, delay, Serial, WheelR, WheelG, WheelB */
 
-// NeoJS Demo ~ sfranzyshen
-// This is a port of strandtest.ino to JavaScript (Elk)
+const COLOR_WIPE_DELAY = 50;
+const THEATER_CHASE_DELAY = 50;
+const RAINBOW_DELAY = 10;
+const THEATER_CHASE_RAINBOW_DELAY = 50;
 
-// colorWipe()
-let colorWipe = function(color, wait) {
-  let i = 0;
-  
-  while(i < Pixel.numPixels()) {
+function colorWipe(color, wait) {
+  for (let i = 0; i < Pixel.numPixels(); i++) {
     Pixel.setPixelColor32(i, color);
     Pixel.show();
     delay(wait);
-	i++;
   }
-};
+}
 
-// theaterChase()
-let theaterChase = function(color, wait) {
-  let a = 0, b = 0, c = 0;
-  
-  while(a < 10) {
-	b = 0;
-    while(b < 3) {
+function theaterChase(color, wait) {
+  for (let a = 0; a < 10; a++) {
+    for (let b = 0; b < 3; b++) {
       Pixel.clear();
-	  c = b;
-      while(c < Pixel.numPixels()) {
+      for (let c = b; c < Pixel.numPixels(); c += 3) {
         Pixel.setPixelColor32(c, color);
-		c += 3;
       }
       Pixel.show();
       delay(wait);
-	  b++;
     }
-	a++;
   }
-};
+}
 
-// rainbow()
-let rainbow = function(wait) {
-  let firstPixelHue = 0, i = 0, pixelHue = 0;
-  
-  while(firstPixelHue < 5 * 65536) {
-	i = 0;
-    while(i < Pixel.numPixels()) {
-      pixelHue = firstPixelHue + (i * 65536 / Pixel.numPixels());
-      Pixel.setPixelColor32(i, Pixel.gamma32(Pixel.ColorHSV(pixelHue)));
-	  i++;
-    }
-    Pixel.show();
-    delay(wait);
-	firstPixelHue += 256;
+function rainbow(wait) {
+  let firstPixelHue = 0;
+  for (let i = 0; i < Pixel.numPixels(); i++) {
+    let pixelHue = firstPixelHue + (i * 65536 / Pixel.numPixels());
+    Pixel.setPixelColor32(i, Pixel.gamma32(Pixel.ColorHSV(pixelHue)));
   }
-};
+  Pixel.show();
+  delay(wait);
+  firstPixelHue += 256;
+}
 
-// theaterChaseRainbow()
-let theaterChaseRainbow = function(wait) {
-  let firstPixelHue = 0, a = 0, b = 0, c = 0, hue = 0, color = 0;
-  
-  while(a < 30) {
-	b = 0;
-    while(b < 3) {
+function theaterChaseRainbow(wait) {
+  let firstPixelHue = 0;
+  for (let a = 0; a < 30; a++) {
+    for (let b = 0; b < 3; b++) {
       Pixel.clear();
-	  c = b;
-      while(c < Pixel.numPixels()) {
-        hue = firstPixelHue + c * 65536 / Pixel.numPixels();
-        color = Pixel.gamma32(Pixel.ColorHSV(hue));
+      for (let c = b; c < Pixel.numPixels(); c += 3) {
+        let hue = firstPixelHue + c * 65536 / Pixel.numPixels();
+        let color = Pixel.gamma32(Pixel.ColorHSV(hue));
         Pixel.setPixelColor32(c, color);
-		c += 3;
       }
       Pixel.show();
       delay(wait);
       firstPixelHue += 65536 / 90;
-	  b++;
     }
-	a++;
   }
-};
+}
 
-// setup()
-let setup = function() {
+function setup() {
   Serial.println("strandtest ... start");
-};
+}
 
-// loop()
-let loop = function() {
-  colorWipe(Pixel.Color(255, 0, 0), 50); // Red
-  colorWipe(Pixel.Color(0, 255, 0), 50); // Green
-  colorWipe(Pixel.Color(0, 0, 255), 50); // Blue
+function loop() {
+  colorWipe(Pixel.Color(255, 0, 0), COLOR_WIPE_DELAY); // Red
+  colorWipe(Pixel.Color(0, 255, 0), COLOR_WIPE_DELAY); // Green
+  colorWipe(Pixel.Color(0, 0, 255), COLOR_WIPE_DELAY); // Blue
 
-  theaterChase(Pixel.Color(127, 127, 127), 50); // White
-  theaterChase(Pixel.Color(127, 0, 0), 50); // Red
-  theaterChase(Pixel.Color( 0, 0, 127), 50); // Blue
+  theaterChase(Pixel.Color(127, 127, 127), THEATER_CHASE_DELAY); // White
+  theaterChase(Pixel.Color(127, 0, 0), THEATER_CHASE_DELAY); // Red
+  theaterChase(Pixel.Color(0, 0, 127), THEATER_CHASE_DELAY); // Blue
 
-  rainbow(10);
-  theaterChaseRainbow(50);
+  rainbow(RAINBOW_DELAY);
+  theaterChaseRainbow(THEATER_CHASE_RAINBOW_DELAY);
+
   Serial.println("strandtest ... loop");
-};
+}
